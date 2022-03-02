51bf7525112e3e6073116cd223b9dfaa55cd6196
Kai Wei Mo <devinkwmo@gmail.com>
2022-03-02 15:25:53 -0500

    Add auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+// import routers
+import { currentUserRouter } from './routes/currentUser';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-	res.send('Hello World!');
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
 	console.log('Server is running on port 3000');
---
+++ auth/src/routes/signin.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const router = express.Router();
+router.post('/api/users/signin', (req, res) => {
+	res.send('hello from signin');
+});
+
+export { router as signinRouter };
